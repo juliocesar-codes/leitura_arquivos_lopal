6b0d6d18c5ed500becc5df3d8ab79b2d3049f846
JULIO CESAR SANTANA ALVES <25132583@dsn127m1246734.cfp127aluno.local>
2025-05-21 15:01:46 -0300

    Adicionando escrever no arquivo

--- src/br/dev/julio/tarefas/Main.java
+++ src/br/dev/julio/tarefas/Main.java
@@ -1,20 +1,55 @@
 package br.dev.julio.tarefas;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Main {
-
+	
+	static String caminho = "/Users/25132583/tarefasDS1TA/tarefas";
+	
 	public static void main(String[] args) {
 		
-		String caminho = "/Users/25132583/tarefasDS1TA/tarefas";
+		escreverArquivo();
+		//lerArquivo();
+		
+		
+	}
+	
+	private static void escreverArquivo() {
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		
+		try {
+			fw = new FileWriter(caminho, true);
+			bw = new BufferedWriter(fw);
+			
+			//Adicionando conteudo no arquivo
+			bw.append("Senai Jandira - ");
+			bw.append("Julio Cesar - ");
+			bw.append("Lógica de Programação\n");
+			
+			//Mandando escrever no arquivo
+			bw.flush();
+			
+			
+		} catch (Exception e) {
+			
+		}
+		
+	}
+
+	private static void lerArquivo() {
+		
 		FileReader fr = null;
 		BufferedReader br = null;
 		
 		try {
 		fr = new FileReader(caminho);
 		br = new BufferedReader(fr);
+		
 		String retorno = br.readLine();
 		
 		while (retorno != null) {
@@ -30,8 +65,6 @@ public class Main {
 		}	
 		
 		System.out.println("Fim");
-		
-		
 	}
 
 }
